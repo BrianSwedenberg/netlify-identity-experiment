2fb1b25f733eeafbbf252fd29d0395e4d993daac
Brian.Swedenberg Swedenberg <bswedenberg@gmail.com>
2023-03-29 15:41:43 +0000

    commented out rest of auth stuff from app

--- src/App.jsx
+++ src/App.jsx
@@ -48,26 +48,26 @@ function AuthExample() {
 //   );
 }
 
-const netlifyAuth = {
-  isAuthenticated: false,
-  user: null,
-  authenticate(callback) {
-    this.isAuthenticated = true;
-    netlifyIdentity.open();
-    netlifyIdentity.on('login', user => {
-      this.user = user;
-      callback(user);
-    });
-  },
-  signout(callback) {
-    this.isAuthenticated = false;
-    netlifyIdentity.logout();
-    netlifyIdentity.on('logout', () => {
-      this.user = null;
-      callback();
-    });
-  }
-};
+// const netlifyAuth = {
+//   isAuthenticated: false,
+//   user: null,
+//   authenticate(callback) {
+//     this.isAuthenticated = true;
+//     netlifyIdentity.open();
+//     netlifyIdentity.on('login', user => {
+//       this.user = user;
+//       callback(user);
+//     });
+//   },
+//   signout(callback) {
+//     this.isAuthenticated = false;
+//     netlifyIdentity.logout();
+//     netlifyIdentity.on('logout', () => {
+//       this.user = null;
+//       callback();
+//     });
+//   }
+// };
 
 // const AuthButton = () => useNavigate(
 //   ({ history }) =>
